b5e981f43d1a3dbfe06f3842c463935c539a1b56
iago-pssjd <40892925+iago-pssjd@users.noreply.github.com>
2021-04-07 13:39:12 +0200

    Change sci-hub address

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 searchSciHub = function(word){
     var query = word.selectionText;
-    chrome.tabs.create({url: "https://sci-hub.tw/" + query});
+    chrome.tabs.create({url: "https://sci-hub.se/" + query});
 };
 
 chrome.contextMenus.create({
